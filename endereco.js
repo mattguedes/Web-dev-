d4c1009ac5719f4139ea68ec1d4b4e26e6eb7d01
mattguedes <149540939+mattguedes@users.noreply.github.com>
2024-08-26 16:52:03 -0300

    endereco.js

--- endereco.js
+++ endereco.js
@@ -1,48 +1,50 @@
-'use strict';
+const { Model, DataTypes, Sequelize } = require('sequelize');
+/*const Sequelize = require('sequelize');
+const { type } = require('os');
+const { isNumberObject } = require('util/types');*/
+const sequelize = new Sequelize('postgres://postgres:banco123@localhost:5432/apinode');
 
-/** @type {import('sequelize-cli').Migration} */
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('enderecos', {
-      Id: {
-        type: Sequelize.BIGINT,
-        primaryKey: true,
-        autoIncrement: true,
-        allowNull: false
-      },
-      Cep: {
-          type: Sequelize.STRING,
-          allowNull: false
-      },
-      Numero: {
-          type: Sequelize.INTEGER,
-          allowNull: false
-      },
-      Complemento: {
-          type: Sequelize.STRING,
-          allowNull: false
-      },
-      Bairro: {
-          type: Sequelize.STRING,
-          allowNull: false
-      },
-      Cidade: {
-          type: Sequelize.STRING,
-          allowNull: false
-      },
-      Estado: {
-          type: Sequelize.STRING,
-          allowNull: false
-      },
-      MunicipioIBGE: {
-          type: Sequelize.STRING,
-          allowNull: false
-      },
+class Endereco extends Model {}
+
+Endereco.init({
+        Id: {
+            type: DataTypes.BIGINT,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        Cep: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Numero: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        Complemento: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Bairro: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Cidade: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        Estado: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        MunicipioIBGE: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+    }, {
+        sequelize,
+        modelName: 'Endereco',
+        tableName: 'enderecos',
+        timestamps: true
     });
-  },
 
-
-  async down (queryInterface, Sequelize) {
-    await queryInterface.dropTable('enderecos');
-  }
-};
+    module.exports = Endereco;
